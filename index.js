d98fa47578ff2721b19006977ee2457de7ce4333
github username <github email address>
2021-11-09 09:50:58 -0500

    commit tree b20770393f8cf9f1a57ae2e2a84352d70352d843

---
+++ index.js
@@ -0,0 +1,12 @@
+// let divide = function() {
+//     return 2000 / 100;
+// }
+
+let divide = () => 2000/100
+
+let square = (num) => num * num;
+
+let add = (a,b) => a + b;
+
+// Variation with {}, requires explicit return!
+// let add = (a,b) => {return a + b};
